0895fbfcb7e2a112868cbe9f0fa130dad9a256a5
Setu Marathe <setu.marathe@gmail.com>
2025-12-05 21:33:50 -0500

    Docs

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/my-portfolio/',
+  base: '/my-portfolio/',   // <- MUST be correct
   build: {
-    outDir: 'docs'
+    outDir: 'docs',        // <- HERE is the line you're asking for
   }
 })
